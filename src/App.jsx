e48ff7ee4c53cd7fe3aa11af12b1a317e6dfdc72
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-01-16 18:49:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app-container">
+
+      <p className='title1'>Generate a</p>
+      <p className='title2'>random password</p>
+      <p className='subtitle'>Never use an insecure password again.</p>
+
+      <div className='btn-generate'>
+        <button className='btn-generate-text'>Generate passwords</button>
+      </div>
+
+      <div className='line'/>
+
+      <div className='btn-password'>
+        <p className='btn-password-text'>testee474!dsds</p>
+        <p className='btn-password-text'>@test474!dsds</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
